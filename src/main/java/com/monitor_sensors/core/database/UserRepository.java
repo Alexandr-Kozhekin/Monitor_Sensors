87dd875594792fbbdd6e33ddef26c124fc66a91d
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2022-09-02 22:00:23 +0300

    added UserRepository and implementation

---
+++ src/main/java/com/monitor_sensors/core/database/UserRepository.java
@@ -0,0 +1,14 @@
+package com.monitor_sensors.core.database;
+
+import com.monitor_sensors.core.domain.User;
+
+import java.util.List;
+
+public interface UserRepository {
+
+    void registrationUser(User user);
+
+    User authorizationUser(String email, char[] password);
+
+    List<User> fiendAllUsers();
+}
---
+++ src/main/java/com/monitor_sensors/core/database/UserRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.monitor_sensors.core.database;
+
+import com.monitor_sensors.core.domain.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@Transactional
+public class UserRepositoryImpl implements UserRepository{
+
+    @Autowired private SessionFactory sessionFactory;
+
+    public void registrationUser(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    public User authorizationUser(String email, char[] password) {
+
+        Query query = sessionFactory.getCurrentSession()
+                .createQuery("SELECT u FROM User u WHERE email = :email and password = :password");
+        query.setParameter("email", email);
+        query.setParameter("password", password);
+        query.getResultList();
+
+        return (User)query.getResultList().get(0);
+    }
+
+    public List<User> fiendAllUsers() {
+
+        return sessionFactory
+                .getCurrentSession()
+                .createQuery("SELECT u FROM User u", User.class)
+                .getResultList();
+
+    }
+
+}
