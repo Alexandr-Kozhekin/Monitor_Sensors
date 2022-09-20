2f317c66ee1e9e0e9529a3138dfa880a8fab6b2f
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2022-09-20 20:24:47 +0300

    refactoring

--- src/main/java/com/monitor_sensors/core/database/SensorRepositoryImpl.java
+++ src/main/java/com/monitor_sensors/core/database/SensorRepositoryImpl.java
@@ -1,195 +1,157 @@
 package com.monitor_sensors.core.database;
 
+import com.monitor_sensors.core.database.RowMapper.SensorRowMapper;
 import com.monitor_sensors.core.domain.Sensor;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
 @Component
-@Transactional
 public class SensorRepositoryImpl implements SensorRepository {
 
-    @Autowired private SessionFactory sessionFactory;
+    @Autowired private JdbcTemplate jdbcTemplate;
 
     @Override
     public void saveSensor(Sensor sensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createSQLQuery("INSERT INTO SENSORS (title, model, range_from, range_to, type, unit, location, description) " +
-                        "VALUES (:title, :model, :rangeFrom, :rangeTo, :type, :unit, :location, :description); ");
+        String sql = "INSERT INTO SENSORS (title, model, range_from, range_to, type, unit, location, description) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-        query.setParameter("title", sensor.getTitle());
-        query.setParameter("model", sensor.getModel());
-        query.setParameter("rangeFrom", sensor.getRangeFrom());
-        query.setParameter("rangeTo", sensor.getRangeTo());
-        query.setParameter("type", sensor.getType());
-        query.setParameter("unit", sensor.getUnit());
-        query.setParameter("location", sensor.getLocation());
-        query.setParameter("description", sensor.getDescription());
-
-        query.executeUpdate();
+        jdbcTemplate.update(sql, sensor.getTitle(), sensor.getModel(), sensor.getRangeFrom(),
+                sensor.getRangeTo(), sensor.getType(), sensor.getUnit(), sensor.getLocation(),
+                sensor.getDescription());
 
     }
 
     @Override
     public ArrayList<Sensor> findAllSensor() {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createSQLQuery("SELECT id, title, model FROM SENSORS");
+        String sql = "SELECT id, title, model FROM SENSORS";
 
-        return (ArrayList<Sensor>) query.getResultList();
+        return (ArrayList<Sensor>) jdbcTemplate.query(sql, new SensorRowMapper());
     }
 
     @Override
     public ArrayList<Sensor> findAllSensorByParam(String[] param) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createSQLQuery("SELECT * FROM SENSORS WHERE " +
-                        "title = :title OR " +
-                        "model = :model OR " +
-                        "range_from = :range_from OR " +
-                        "range_to = :range_to OR " +
-                        "type = :type OR " +
-                        "unit = :unit OR " +
-                        "location = :location OR " +
-                        "description = :description");
+        String sql = "SELECT * FROM SENSORS WHERE (title LIKE ?) OR (model LIKE ?) " +
+                "OR (range_from LIKE ?) OR (range_to LIKE ?) OR (type LIKE ?) " +
+                "OR (unit LIKE ?) OR (location LIKE ?) OR (description LIKE ?)";
 
-        query.setParameter("title", param[0]);
-        query.setParameter("model", param[1]);
-        query.setParameter("range_from", param[2]);
-        query.setParameter("range_to", param[3]);
-        query.setParameter("type", param[4]);
-        query.setParameter("unit", param[5]);
-        query.setParameter("location", param[6]);
-        query.setParameter("description", param[7]);
-
-        return (ArrayList<Sensor>) query.getResultList();
+        return (ArrayList<Sensor>) jdbcTemplate.query(sql, param, new SensorRowMapper());
     }
 
     @Override
     public ArrayList<Sensor> findAllSensorByTitle(String title) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("SELECT s FROM Sensor s WHERE title = :title", Sensor.class);
+        String sql = "SELECT * FROM SENSORS WHERE title = ?";
 
-        query.setParameter("title", title);
+        Object[] args = new Object[] {title};
 
-        return (ArrayList<Sensor>) query.getResultList();
+        return (ArrayList<Sensor>) jdbcTemplate.query(sql, args, new SensorRowMapper());
     }
 
     @Override
     public ArrayList<Sensor> findAllSensorByModel(String model) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("SELECT s FROM Sensor s WHERE model = :model", Sensor.class);
+        String sql = "SELECT * FROM SENSORS WHERE model = ?";
 
-        query.setParameter("model", model);
+        Object[] args = new Object[] {model};
 
-        return (ArrayList<Sensor>) query.getResultList();
+        return (ArrayList<Sensor>) jdbcTemplate.query(sql, args, new SensorRowMapper());
     }
 
     @Override
     public boolean updateTitleSensorById(String updateTitle, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET title = :updateTitle WHERE id = :idSensor")
-                .setParameter("updateTitle", updateTitle)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET title = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateTitle, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean updateModelSensorById(String updateModel, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET model = :updateModel WHERE id = :idSensor")
-                .setParameter("updateModel", updateModel)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET model = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateModel, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean updateRangeFromSensorById(int updateRangeFrom, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET rangeFrom = :updateRangeFrom WHERE id = :idSensor")
-                .setParameter("updateRangeFrom", updateRangeFrom)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET range_from = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateRangeFrom, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean updateRangeToSensorById(int updateRangeTo, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET rangeTo = :updateRangeTo WHERE id = :idSensor")
-                .setParameter("updateRangeTo", updateRangeTo)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET range_to = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateRangeTo, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean updateTypeSensorById(String updateType, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET type = :updateType WHERE id = :idSensor")
-                .setParameter("updateType", updateType)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET type = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateType, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean updateUnitSensorById(String updateUnit, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET unit = :updateUnit WHERE id = :idSensor")
-                .setParameter("updateUnit", updateUnit)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET unit = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateUnit, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean updateLocationSensorById(String updateLocation, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET location = :updateLocation WHERE id = :idSensor")
-                .setParameter("updateLocation", updateLocation)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET location = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateLocation, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean updateDescriptionSensorById(String updateDescription, Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("UPDATE Sensor SET description = :updateDescription WHERE id = :idSensor")
-                .setParameter("updateDescription", updateDescription)
-                .setParameter("idSensor", idSensor);
+        String sql = "UPDATE SENSORS SET description = ? WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {updateDescription, idSensor};
+
+        return jdbcTemplate.update(sql, args, new SensorRowMapper()) == 1;
     }
 
     @Override
     public boolean deleteSensorById(Long idSensor) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("DELETE Sensor WHERE id = :idSensor");
-        query.setParameter("idSensor", idSensor);
+        String sql = "DELETE SENSORS WHERE id = ?";
 
-        return query.executeUpdate() == 1;
+        Object[] args = new Object[] {idSensor};
+
+        return jdbcTemplate.update(sql, args) == 1;
     }
 
 }
--- src/main/java/com/monitor_sensors/core/database/UserRepositoryImpl.java
+++ src/main/java/com/monitor_sensors/core/database/UserRepositoryImpl.java
@@ -1,54 +1,52 @@
 package com.monitor_sensors.core.database;
 
+import com.monitor_sensors.core.database.RowMapper.UserRowMapper;
 import com.monitor_sensors.core.domain.User;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component
-@Transactional
 public class UserRepositoryImpl implements UserRepository{
 
-    @Autowired private SessionFactory sessionFactory;
+    @Autowired private JdbcTemplate jdbcTemplate;
 
     public void registrationUser(User user) {
-        sessionFactory.getCurrentSession().save(user);
+
+        String sql = "INSERT INTO USERS (role, state, email, password) VALUES (?, ?, ?, ?)";
+
+        jdbcTemplate.update(sql, user.getUserRole().toString(), user.getUserState().toString(),
+                user.getEmail(), String.valueOf(user.getPassword()));
+
     }
 
     public User authorizationUser(String email, char[] password) {
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("SELECT u FROM User u WHERE email = :email and password = :password");
-        query.setParameter("email", email);
-        query.setParameter("password", password);
-        query.getResultList();
+        String sql = "SELECT * FROM USERS WHERE email = ? AND password = ?";
 
-        return (User) query.getResultList().get(0);
+        Object[] args = new Object[] {email, String.valueOf(password)};
+
+        return jdbcTemplate.query(sql, args, new UserRowMapper()).get(0);
     }
 
     public List<User> fiendAllUsers() {
 
-        return sessionFactory
-                .getCurrentSession()
-                .createQuery("SELECT u FROM User u", User.class)
-                .getResultList();
+        String sql = "SELECT * FROM USERS";
+
+        return jdbcTemplate.query(sql, new UserRowMapper());
 
     }
 
     public User findUserByEmail(String email){
 
-        Query query = sessionFactory.getCurrentSession()
-                .createQuery("SELECT u FROM User u WHERE email = :email", User.class);
+        String sql = "SELECT * FROM USERS WHERE email = ?";
 
-        query.setParameter("email", email);
+        Object[] args = new Object[] {email};
 
-        return (User) query.getResultList().get(0);
+        return jdbcTemplate.query(sql, args, new UserRowMapper()).get(0);
     }
 
 }
